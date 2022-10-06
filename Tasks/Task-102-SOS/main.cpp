bff0c63064c90b71d7ae8d510065d1ba5a994d21
Nidu Rasnayake <106425621+Raptor2718@users.noreply.github.com>
2022-10-06 15:08:19 +0100

    Happy Birthday!!! ;)

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -7,6 +7,33 @@ DigitalOut greenLED(TRAF_GRN1_PIN);
 Buzzer buzz;
 Buttons buttons;
 
+void play(int n, int secs)
+{
+    greenLED = 1;
+
+    switch (n)
+    {
+        case 1: buzz.playTone("C");
+        break;
+        case 2: buzz.playTone("D");
+        break;
+        case 3: buzz.playTone("E");
+        break;
+        case 4: buzz.playTone("F");
+        break;
+        case 5: buzz.playTone("G");
+        break;
+        case 6: buzz.playTone("A", Buzzer::HIGHER_OCTAVE);
+        break;
+        case 7: buzz.playTone("B", Buzzer::HIGHER_OCTAVE);
+        break;
+    }
+    wait_us(WAIT_TIME_MS * (secs - 0.1));
+
+    buzz.rest();
+    wait_us(WAIT_TIME_MS * 0.1);
+};
+
 // TIP: (I suggest you read this!)
 //
 // Press the black reset button to restart the code (and stop the sound)
@@ -20,37 +47,38 @@ int main()
     while (true)
     {
 
-        
-        //On for 500ms
-        greenLED = 1;
-        buzz.playTone("C");
+        play(1, 2);
+        play(1, 1);
+        play(2, 2);
+        play(1, 2);
+        play(4, 2);
+        play(3, 2);
         wait_us(WAIT_TIME_MS * 2);
+        play(1, 2);
+        play(1, 1);
+        play(2, 2);
+        play(1, 2);
+        play(5, 2);
+        play(4, 2);
+        wait_us(WAIT_TIME_MS * 2);
+        play(1, 2);
+        play(1, 2);
+        play(6, 2);
+        play(5, 2);
+        play(3, 2);
+        play(2, 2);
+        wait_us(WAIT_TIME_MS * 2);
+        play(6, 4);
+        play(4, 2);
+        play(5, 4);
+        play(4, 4);
+
+
         
-        greenLED = 1;
-        buzz.playTone("C");
-        wait_us(WAIT_TIME_MS *1);
-
-        greenLED = 1;
-        buzz.playTone("D");
-        wait_us(WAIT_TIME_MS *2);
-
-        greenLED = 1;
-        buzz.playTone("C");
-        wait_us(WAIT_TIME_MS *2);
-
-        greenLED = 1;
-        buzz.playTone("F");
-        wait_us(WAIT_TIME_MS *2);
-
-        greenLED = 1;
-        buzz.playTone("E");
-        wait_us(WAIT_TIME_MS *1);
-
-
         
         //Pause
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 1000);
+        wait_us(WAIT_TIME_MS * 4);
 
     }
 }
