8268ce160db381c64c0f29720d6c7da110cdc2be
Nidu Rasnayake <106425621+Raptor2718@users.noreply.github.com>
2022-10-23 19:44:12 +0100

    all good

--- Tasks/Task-127-binary_display/main.cpp
+++ Tasks/Task-127-binary_display/main.cpp
@@ -1,16 +1,20 @@
 #include "mbed.h"
+#include "uop_msb.h"
 using namespace uop_msb;
 
+LCD_16X2_DISPLAY lcd;
 
 //BusOut lights(PC_2, PC_3, PC_6);
 BusIn buttons(PG_0, PG_1, PG_2, PG_3);
-LCD_16X2_DISPLAY lcd;
+int num;
 
-// main() runs in its own thread in the OS
 int main()
 {
     buttons[2].mode(PinMode::PullDown);
     buttons[3].mode(PinMode::PullDown);
+
+    lcd.cls();
+    lcd.locate(0, 0);
 
     //lights = 0;
 
@@ -19,6 +23,9 @@ int main()
         while (buttons == 0);
         wait_us(10000);
 
+        num = buttons;
+
+        lcd.printf("%i ", num);
         //lights = buttons;
 
         while (buttons != 0);
